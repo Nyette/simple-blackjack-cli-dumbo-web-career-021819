91dbd3c1600fe4ac1f447830ca20fa1f6cd5ffb8
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 00:49:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,7 +66,7 @@ end
 def runner
   # code runner here
   welcome
-  initial_round
+  card_total = initial_round
   until card_total > 21 do
     hit?(card_total)
     display_card_total(card_total)
